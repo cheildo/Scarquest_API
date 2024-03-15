2aaacaba39f8303cb7e9f151c2c884e335ffde80
cheildo <cheickzida@gmail.com>
2024-03-15 15:18:17 +0800

    get circulating supply from the blockchain, add gettotalSupply endpoint; and document README.md

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # CoinMarketCap_API
-API endpoint to return the circulating supply for Scarquest SCAR token
+An API endpoint to return the circulating supply for Scarquest SCAR token
+
+# How to install
+npm install
+npm start
+
+# How it work
+To get the circulating supply
+http://yourdomain.com/api/scar/circulating
+
+To get the total supply
+http://yourdomain.com/api/scar/total
+
--- index.js
+++ index.js
@@ -1,18 +1,63 @@
+const Web3 = require("web3");
 const express = require('express');
+
+const {treasuryAddress, scarAddress, scarABI} = require('./config/config');
+
 const app = express();
 const PORT = process.env.PORT || 3000;
+const HOST_DOMAIN = 'yourdomain.com';
 
-function getCirculatingPrice() {
-  return Math.random() * 100000000;
+//const provider = new Web3.providers.WebsocketProvider(`wss://bsc-rpc.publicnode.com`)
+const provider = new Web3.providers.HttpProvider('https://bsc-dataseed1.binance.org/')
+const web3 = new Web3(provider);
+const scarContract = new web3.eth.Contract(scarABI, scarAddress);
+
+async function getCirculatingSupply() {
+
+    const totalSupply = await getTotalSupply()
+
+    const treasuryBalance = await scarContract.methods.balanceOf(treasuryAddress).call()
+    .then(async function(balance){
+        const treasuryToWei = web3.utils.fromWei(balance , 'ether')
+        return treasuryToWei
+    } )
+    .catch(error => {
+        console.error(error)
+    });
+    console.log("treasury balance: ", treasuryBalance)
+
+    const circulating = totalSupply - treasuryBalance
+    console.log("circulating supply: ", circulating)
+    return circulating;
+
 }
 
-app.get('/api/scar/csupply', (req, res) => {
-  const price = getCirculatingPrice(); 
-  const formattedPrice = price.toFixed(8);
-  res.send(formattedPrice);
+async function getTotalSupply() {
+    const totalSupply = await scarContract.methods.totalSupply().call()
+    .then(async function(balance){
+        const totalToWei = web3.utils.fromWei(balance , 'ether')
+        return totalToWei
+    } )
+    .catch(error => {
+        console.error(error)
+    });
+    console.log("total supply: ", totalSupply)
+    return totalSupply
+}
+
+app.get('/api/scar/circulating', async (req, res) => {
+    const circulatingSupply = await getCirculatingSupply();
+    const formattedCirculatingSupply = circulatingSupply.toString()
+    res.send(formattedCirculatingSupply);
 });
+
+app.get('/api/scar/total', async (req, res) => {
+    const totalSupply = await getTotalSupply(); 
+    const formattedTotalSupply = totalSupply.toString();
+    res.send(formattedTotalSupply);
+  });
 
 
 app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+    console.log(`Server is running on http://${HOST_DOMAIN}:${PORT}`);
 });
